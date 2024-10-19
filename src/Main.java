2e3f5750d0185f4b388aa3e9119b25b35a93bb2b
Sergej <luzhins764@gmail.com>
2024-10-19 13:53:11 +0300

    working version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        WeatherService weatherService = new WeatherService();
+        weatherService.getData();
+        weatherService.printTemperature();
     }
 }
